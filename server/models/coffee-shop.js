489072568905a5c3585affd99bf0cbd0101104f0
Taras Mankovski <tarasm@gmail.com>
2015-12-23 17:36:37 -0500

    Fixed indentation

--- server/models/coffee-shop.js
+++ server/models/coffee-shop.js
@@ -1,19 +1,19 @@
 module.exports = function(CoffeeShop) {
     CoffeeShop.status = function(shopId, hour, cb) {
-    CoffeeShop.findById( shopId, function (err, instance) {
-        var opens_at = instance.opens_at;
-        var closes_at = instance.closes_at;
-        console.log('Current hour is ' + hour);
-        var response;
-        if (hour > opens_at && hour < closes_at) {
-          response = 'We are open for business.';
-        } else {
-          response = 'Sorry, we are closed. Open daily from 6am to 8pm.';
-        }
-        cb(null, response);
-        console.log(response);
-    });
-  };
+      CoffeeShop.findById( shopId, function (err, instance) {
+          var opens_at = instance.opens_at;
+          var closes_at = instance.closes_at;
+          console.log('Current hour is ' + hour);
+          var response;
+          if (hour > opens_at && hour < closes_at) {
+            response = 'We are open for business.';
+          } else {
+            response = 'Sorry, we are closed. Open daily from 6am to 8pm.';
+          }
+          cb(null, response);
+          console.log(response);
+      });
+    };
   
   CoffeeShop.getName = function(shopId, cb) {
     CoffeeShop.findById( shopId, function (err, instance) {

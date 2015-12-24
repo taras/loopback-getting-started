92f1e77db5836885cadaafa23b61eff81c19279b
Taras Mankovski <tarasm@gmail.com>
2015-12-24 11:38:16 -0500

    Added /coffeshops/{id} test

--- test/coffeeshops-test.js
+++ test/coffeeshops-test.js
@@ -1,4 +1,3 @@
-var bootServer = require('./helpers/boot-server');
 var assert = require('assert');
 
 describe('/coffeeshops', function() {
@@ -11,4 +10,23 @@ describe('/coffeeshops', function() {
         });
     });
   });
+});
+
+describe('/coffeeshops/{id}', function(){
+    describe('GET', function(){
+       it('should return Bel Cafe for id=1', function(){
+          return chai.request(app).get('/api/coffeeshops/1')
+            .then(function(res){
+                assert.equal(res.status, 200, 'status is 200');
+                assert.equal(res.body.name, 'Bel Cafe');
+            });
+       });
+       
+       it('should return a 404 for id=4', function(){
+          return chai.request(app).get('/api/coffeeshops/4')
+            .then(function(res){
+               assert.equal(res.status, 404, 'status is 404'); 
+            });
+       });
+    });
 });

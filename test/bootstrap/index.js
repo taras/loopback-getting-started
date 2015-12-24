b432da2ff4334b0d2733b6cd4df94f1e389e63cf
Taras Mankovski <tarasm@gmail.com>
2015-12-24 11:18:34 -0500

    Simplified test setup

--- test/bootstrap/index.js
+++ test/bootstrap/index.js
@@ -1,7 +1,14 @@
-var chai = require('chai');
+var loopback = require('loopback');
+var boot = require('loopback-boot');
+var path = require('path');
 
 global.chai = require('chai');
 
 var chaiHTTP = require('chai-http');
 
 global.chai.use(require('chai-http'));
+
+global.app = loopback();
+var serverPath = path.join(process.cwd(), 'server');
+
+boot(global.app, serverPath);
--- test/coffeeshopts-test.js
+++ test/coffeeshopts-test.js
@@ -2,13 +2,9 @@ var bootServer = require('./helpers/boot-server');
 var assert = require('assert');
 
 describe('/coffeeshopts', function() {
-  
   describe('GET', function () {
     it('should return 3 coffeeshops', function () {
-      return bootServer()
-        .then(function(app){
-          return chai.request(app).get('/api/coffeeshops');
-        })
+      return chai.request(app).get('/api/coffeeshops')
         .then(function(res){
           assert.equal(res.status, 200, 'status is 200');
           assert.equal(res.body.length, 3, 'there are 3 items');

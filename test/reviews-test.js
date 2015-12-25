5de3f10186cf665c3366f8cdfe9e83989d71f712
Taras Mankovski <tarasm@gmail.com>
2015-12-24 22:23:22 -0500

    Added permissions and test for review

--- test/reviews-test.js
+++ test/reviews-test.js
@@ -9,4 +9,28 @@ describe('reviews', function(){
         });
     });
   });
+  
+  describe('only logged in users can edit or delete a review', function(){
+    it('should return a 401 for unauthenticated update request', function(){
+      return chai.request(app)
+        .put('/api/reviews/1')
+        .send({ comments: 'foo' })
+        .then(function(res){
+          assert.notEqual(res.status, 200, 'Should not be 200');
+        })
+        .catch(function(res){
+          assert.equal(res.status, 401, 'Anonymous user should get a 401');
+        });
+    });
+    it('should return a 401 for unauthenticated delete request', function(){
+      return chai.request(app)
+        .delete('/api/reviews/1')
+        .then(function(res){
+          assert.notEqual(res.status, 200, 'Should not be 200');
+        })
+        .catch(function(res){
+          assert.equal(res.status, 401, 'Anonymous user should get a 401');
+        });
+    });
+  });
 });

5f71bba63612b5d8c7f4404bd3beec443aec53d8
Taras Mankovski <tarasm@gmail.com>
2015-12-24 22:35:34 -0500

    Added remote hook at adds date and publisherId

--- server/models/review.js
+++ server/models/review.js
@@ -1,3 +1,10 @@
-module.exports = function(Review) {
+"use strict";
 
+module.exports = function(Review) {
+  Review.beforeRemote('create', function(context, user, next){
+    let req = context.req;
+    req.body.date = Date.now();
+    req.body.publishId = req.accessToken.userId;
+    next();
+  });
 };
